9d72c96938e3d58100cbc73885ba1adf294eba4f
JishnuVijayan <jishnu03ptb@gmail.com>
2026-03-05 22:08:00 +0530

    fix: minor changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "standalone",
+  experimental: {
+    turbo: undefined,
+  },
 };
 
 export default nextConfig;
